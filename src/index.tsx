6686a2c3144017b870baed25de47fc414deb7a17
yewon97 <dpdnjs9936@gmail.com>
2024-06-09 16:33:46 +0900

    react router dom 설정 완료

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,50 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    index: true,
+    element: <App />,
+  },
+  {
+    path: "/posts",
+    element: <h1>Post List Page</h1>,
+  },
+  {
+    path: "/posts/:id",
+    element: <h1>ID Detail Page</h1>,
+  },
+  {
+    path: "/posts/new",
+    element: <h1>new post Detail Page</h1>,
+  },
+  {
+    path: "/posts/edit/:id",
+    element: <h1>Edit Page</h1>,
+  },
+  {
+    path: "/profile",
+    element: <h1>Profile page</h1>,
+  },
+  {
+    path: "*",
+    element: <Navigate replace to="/" />,
+  },
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
